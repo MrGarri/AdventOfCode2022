0feaf1346f09c586780e189340ae9a3021f9e2fc
Roberto 🎸 Garrido <robgrri@amazon.com>
2022-12-09 14:22:02 +0100

    Add day 9 skeleton

---
+++ src/main/java/day9/RopeBridge.java
@@ -0,0 +1,26 @@
+package day9;
+
+import common.Puzzle;
+
+import java.util.List;
+
+public class RopeBridge extends Puzzle {
+    public RopeBridge(List<String> puzzleInput) {
+        super(puzzleInput);
+    }
+
+    @Override
+    public Object solvePart1() {
+        return null;
+    }
+
+    @Override
+    public Object solvePart2() {
+        return null;
+    }
+
+    @Override
+    public int getDay() {
+        return 0;
+    }
+}
